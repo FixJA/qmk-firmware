9847cb81729fa6540ffed1a583b9acdaaa20607b
Evgeny K <3193631@gmail.com>
2026-07-24 14:04:39 +0200

    chore(ryodeushii): cleanup custom keymaps from L7

--- keyboards/nuphy/air75v2/ansi/keymaps/ryodeushii/keymap.c
+++ keyboards/nuphy/air75v2/ansi/keymaps/ryodeushii/keymap.c
@@ -17,7 +17,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 // layer Mac Fn
 [1] = LAYOUT_ansi_84(
-	TOG_BAT_IND_NUM, 	KC_F1,  	KC_F2,  	KC_F3, 		KC_F4,  	KC_F5,  	KC_F6,  	KC_F7,  	KC_F8,  	KC_F9, 		KC_F10, 	KC_F11, 	KC_F12, 	MAC_PRT,	KC_F14,	    TG(7),
+	TOG_BAT_IND_NUM, 	KC_F1,  	KC_F2,  	KC_F3, 		KC_F4,  	KC_F5,  	KC_F6,  	KC_F7,  	KC_F8,  	KC_F9, 		KC_F10, 	KC_F11, 	KC_F12, 	MAC_PRT,	KC_F14,	    KC_INS,
 	_______, 	LNK_BLE1,  	LNK_BLE2,  	LNK_BLE3,  	LNK_RF,   	_______,   	_______,   	_______,   	_______,   	_______,  	_______,   	_______,	_______, 	_______,	            _______,
 	_______, 	_______,   	_______,   	_______,   	_______,   	_______,   	DEBOUNCE_PRESS_DEC,   	DEBOUNCE_PRESS_SHOW,   	DEBOUNCE_PRESS_INC,   	TOG_DEEP_SLEEP,  	TOG_USB_SLP,   	DEV_RESET,	SLEEP_MODE, BAT_SHOW,	            _______,
 	TOG_CAPS_IND,	SLEEP_TIMEOUT_DEC,   	SLEEP_TIMEOUT_SHOW,   	SLEEP_TIMEOUT_INC,  	_______,   	_______,   	DEBOUNCE_RELEASE_DEC,	DEBOUNCE_RELEASE_SHOW,   	DEBOUNCE_RELEASE_INC,   	_______,  	_______,	_______, 	_______,	                            _______,
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 // layer win Fn
 [3] = LAYOUT_ansi_84(
-	TOG_BAT_IND_NUM, 	KC_BRID,  	KC_BRIU,  	_______, 	    _______,  	_______,  	_______,  	KC_MPRV,  	KC_MPLY,  	KC_MNXT, 	KC_MUTE, 	KC_VOLD, 	KC_VOLU, 	KC_PSCR,	KC_F14,		TG(7),
+	TOG_BAT_IND_NUM, 	KC_BRID,  	KC_BRIU,  	_______, 	    _______,  	_______,  	_______,  	KC_MPRV,  	KC_MPLY,  	KC_MNXT, 	KC_MUTE, 	KC_VOLD, 	KC_VOLU, 	KC_PSCR,	KC_F14,		KC_INS,
 	_______, 	LNK_BLE1,  	LNK_BLE2,  	LNK_BLE3,  	LNK_RF,   	_______,   	_______,   	_______,   	_______,   	_______,  	_______,   	_______,	_______, 	_______,	            _______,
 	_______, 	_______,   	_______,   	_______,   	_______,   	_______,   	DEBOUNCE_PRESS_DEC,   	DEBOUNCE_PRESS_SHOW,   	DEBOUNCE_PRESS_INC,   	TOG_DEEP_SLEEP,  	TOG_USB_SLP,   	DEV_RESET,	SLEEP_MODE, BAT_SHOW,	            _______,
 	TOG_CAPS_IND,	SLEEP_TIMEOUT_DEC,   	SLEEP_TIMEOUT_SHOW,   	SLEEP_TIMEOUT_INC,  	_______,   	_______,   	DEBOUNCE_RELEASE_DEC,	DEBOUNCE_RELEASE_SHOW,   	DEBOUNCE_RELEASE_INC,   	_______,  	_______,	_______, 	_______,	                            _______,
@@ -61,12 +61,5 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	_______,	_______,	_______,						_______, 							_______,	MO(5),   	_______,				AMBIENT_MOD,	AMBIENT_VAD,   AMBIENT_HUI),
 
 
-[7] = LAYOUT_ansi_84(
-	_______, 	_______,  	_______,  	_______, 	_______,  	_______,  	_______,  	_______,  	_______,  	_______, 	_______, 	_______, 	_______, 	_______,	_______,    TG(7),
-	_______, 	_______,   	_______,   	_______,  	_______,   	_______,   	_______,   	_______,   	_______,   	_______,  	_______,   	_______,	_______, 	_______,	            _______,
-	KC_F24, 	_______,  	_______,  	_______,  	_______,   	_______,   	_______,   	_______,   	_______,   	_______,  	_______,   	_______,	_______, 	_______,	            _______,
-	_______,	_______,   	_______,   	_______,  	_______,   	_______,   	_______,	_______,   	_______,   	_______,  	_______,	_______, 	_______,	                            _______,
-	_______,				_______,   	_______,   	_______,  	_______,   	_______,   	_______,	_______, 	_______,	_______,	_______,	_______,	            _______,    _______,
-	_______,	_______,	_______,						_______, 							_______,	_______,   	_______,                _______,    _______,	_______),
 };
 // clang-format on
--- keyboards/nuphy/gem80/ansi/keymaps/ryodeushii/keymap.c
+++ keyboards/nuphy/gem80/ansi/keymaps/ryodeushii/keymap.c
@@ -19,7 +19,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // layer Mac Fn
 #if(WORK_MODE == THREE_MODE)
 [1] = LAYOUT_tkl_f13_ansi(
-	TOG_BAT_IND_NUM, 	KC_F1,  	KC_F2,  	KC_F3, 		KC_F4,  	KC_F5,  	KC_F6,  	KC_F7,  	KC_F8,  	KC_F9, 		KC_F10, 	KC_F11, 	KC_F12, 	TG(7),	    MAC_PRT,    _______,		_______,
+	TOG_BAT_IND_NUM, 	KC_F1,  	KC_F2,  	KC_F3, 		KC_F4,  	KC_F5,  	KC_F6,  	KC_F7,  	KC_F8,  	KC_F9, 		KC_F10, 	KC_F11, 	KC_F12, 	KC_INS,	    MAC_PRT,    _______,		_______,
 	_______, 	LNK_BLE1,  	LNK_BLE2,  	LNK_BLE3,  	LNK_RF,   	_______,   	_______,   	_______,   	_______,   	_______,  	_______,   	_______,	_______,    _______, 	    DEBOUNCE_PRESS_DEC,	    DEBOUNCE_PRESS_SHOW,	DEBOUNCE_PRESS_INC,
     _______, 	_______,   	_______,   	_______,   	_______,   	_______,   	_______,   	_______,   	_______,   	TOG_DEEP_SLEEP,  	TOG_USB_SLP,   	DEV_RESET,	SLEEP_MODE, BAT_SHOW,	DEBOUNCE_RELEASE_DEC,	DEBOUNCE_RELEASE_SHOW,	DEBOUNCE_RELEASE_INC,
 	TOG_CAPS_IND,	SLEEP_TIMEOUT_DEC,   	SLEEP_TIMEOUT_SHOW,   	SLEEP_TIMEOUT_INC,  	_______,   	_______,   	_______,	_______,   	_______,   	_______,  	_______,	_______, 	 			_______,
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	_______,	_______,	_______,										_______, 							_______,	_______,   	MO(1),		_______,				RM_NEXT,    RM_VALD,	RM_HUEU),
 #else
 [1] = LAYOUT_tkl_f13_ansi(
-	_______, 	KC_F1,  	KC_F2,  	KC_F3, 		KC_F4,  	KC_F5,  	KC_F6,  	KC_F7,  	KC_F8,  	KC_F9, 		KC_F10, 	KC_F11, 	KC_F12, 	TG(7),	    MAC_PRT,    _______,		_______,
+	_______, 	KC_F1,  	KC_F2,  	KC_F3, 		KC_F4,  	KC_F5,  	KC_F6,  	KC_F7,  	KC_F8,  	KC_F9, 		KC_F10, 	KC_F11, 	KC_F12, 	KC_INS,	    MAC_PRT,    _______,		_______,
 	_______,    _______,    _______,    _______,    _______,  	_______,   	_______,   	_______,   	_______,   	_______,  	_______,   	_______,	_______,    _______, 	    DEBOUNCE_PRESS_DEC,	    DEBOUNCE_PRESS_SHOW,	DEBOUNCE_PRESS_INC,
     _______, 	_______,   	_______,   	_______,   	_______,   	_______,   	_______,   	_______,   	_______,   	_______,  	TOG_USB_SLP,   	DEV_RESET,	SLEEP_MODE, 	_______,	DEBOUNCE_RELEASE_DEC,	DEBOUNCE_RELEASE_SHOW,	DEBOUNCE_RELEASE_INC,
 	TOG_CAPS_IND,	SLEEP_TIMEOUT_DEC,   	SLEEP_TIMEOUT_SHOW,   	SLEEP_TIMEOUT_INC,  	_______,   	_______,   	_______,	_______,   	_______,   	_______,  	_______,	_______, 	 			_______,
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // layer win Fn
 #if(WORK_MODE == THREE_MODE)
 [3] = LAYOUT_tkl_f13_ansi(
-	TOG_BAT_IND_NUM, 	KC_BRID,  	KC_BRIU,  	_______, 	_______,  	_______,  	_______,  	KC_MPRV,  	KC_MPLY,  	KC_MNXT, 	KC_MUTE, 	KC_VOLD, 	KC_VOLU, 	TG(7),	   	KC_PSCR,    KC_TRNS,    WIN_LOCK,
+	TOG_BAT_IND_NUM, 	KC_BRID,  	KC_BRIU,  	_______, 	_______,  	_______,  	_______,  	KC_MPRV,  	KC_MPLY,  	KC_MNXT, 	KC_MUTE, 	KC_VOLD, 	KC_VOLU, 	KC_INS,	   	KC_PSCR,    KC_TRNS,    WIN_LOCK,
 	_______, 	LNK_BLE1,  	LNK_BLE2,  	LNK_BLE3,  	LNK_RF,   	_______,   	_______,   	_______,   	_______,   	_______,  	_______,   	_______,	_______,    _______, 	    DEBOUNCE_PRESS_DEC,	    DEBOUNCE_PRESS_SHOW,	DEBOUNCE_PRESS_INC,
     _______, 	_______,   	_______,   	_______,   	_______,   	_______,   	_______,   	_______,   	_______,   	TOG_DEEP_SLEEP,  	TOG_USB_SLP,   	DEV_RESET,	SLEEP_MODE, BAT_SHOW,	DEBOUNCE_RELEASE_DEC,	DEBOUNCE_RELEASE_SHOW,	DEBOUNCE_RELEASE_INC,
 	TOG_CAPS_IND,	SLEEP_TIMEOUT_DEC,   	SLEEP_TIMEOUT_SHOW,   	SLEEP_TIMEOUT_INC,  	_______,   	_______,   	_______,	_______,   	_______,   	_______,  	_______,	_______, 	 			_______,
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	_______,	_______,	_______,										_______, 							_______,	_______,   	MO(3),		_______,				RM_NEXT,    RM_VALD,	RM_HUEU),
 #else
 [3] = LAYOUT_tkl_f13_ansi(
-	_______, 	KC_BRID,  	KC_BRIU,  	_______, 	_______,  	_______,  	_______,  	KC_MPRV,  	KC_MPLY,  	KC_MNXT, 	KC_MUTE, 	KC_VOLD, 	KC_VOLU, 	TG(7),	   	KC_PSCR,    KC_TRNS,    WIN_LOCK,
+	_______, 	KC_BRID,  	KC_BRIU,  	_______, 	_______,  	_______,  	_______,  	KC_MPRV,  	KC_MPLY,  	KC_MNXT, 	KC_MUTE, 	KC_VOLD, 	KC_VOLU, 	KC_INS,	   	KC_PSCR,    KC_TRNS,    WIN_LOCK,
 	_______,    _______,    _______,    _______,    _______,  	_______,   	_______,   	_______,   	_______,   	_______,  	_______,   	_______,	_______,    _______, 	    DEBOUNCE_PRESS_DEC,	    DEBOUNCE_PRESS_SHOW,	DEBOUNCE_PRESS_INC,
     _______, 	_______,   	_______,   	_______,   	_______,   	_______,   	_______,   	_______,   	_______,   	_______,  	TOG_USB_SLP,   	DEV_RESET,	SLEEP_MODE, 	_______,	DEBOUNCE_RELEASE_DEC,	DEBOUNCE_RELEASE_SHOW,	DEBOUNCE_RELEASE_INC,
 	TOG_CAPS_IND,	SLEEP_TIMEOUT_DEC,   	SLEEP_TIMEOUT_SHOW,   	SLEEP_TIMEOUT_INC,  	_______,   	_______,   	_______,	_______,   	_______,   	_______,  	_______,	_______, 	 			_______,
@@ -76,12 +76,5 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	_______,	_______,   	_______,   	_______,  	_______,   	_______,   	_______,	_______,   	_______,   	_______,  	_______,	_______, 	 			_______,
 	_______,				_______,   	_______,   	_______,  	_______,   	_______,   	_______,	_______, 	AMBIENT_SPD,	AMBIENT_SPI,	_______,				_______,				AMBIENT_VAI,
 	_______,	_______,	_______,										_______, 							_______,	_______,   	MO(5),		_______,				AMBIENT_MOD,   AMBIENT_VAD,	AMBIENT_HUI),
-[7] = LAYOUT_tkl_f13_ansi(
-	_______, 	_______,  	_______,  	_______, 	_______,  	_______,  	_______,  	_______,  	_______,  	_______, 	_______, 	_______, 	_______, 	TG(7),	    _______,	_______,	_______,
-	_______, 	_______,   	_______,   	_______,  	_______,   	_______,   	_______,   	_______,   	_______,   	_______,  	_______,   	_______,	_______, 	_______,	_______,	_______,	_______,
-	KC_F24, 	_______,  	_______,  	_______,  	_______,   	_______,   	_______,   	_______,   	_______,   	_______,  	_______,   	_______,	_______, 	_______,	_______,	_______,	_______,
-	_______,	_______,   	_______,   	_______,  	_______,   	_______,   	_______,	_______,   	_______,   	_______,  	_______,	_______, 	 			_______,
-	_______,				_______,   	_______,   	_______,  	_______,   	_______,   	_______,	_______, 	_______,	_______,	_______,				_______,				_______,
-	_______,	_______,	_______,										_______, 							_______,	_______,   	_______,		_______,			_______,    _______,	_______),
 };
 // clang-format on
